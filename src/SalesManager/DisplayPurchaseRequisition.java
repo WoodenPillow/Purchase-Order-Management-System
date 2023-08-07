e347f4d8a6d0d2880e2660b68c0215eb34f8ba3c
user <user@LAPTOP-F5P2ODKG>
2023-08-07 17:44:46 +0800

    Display Purchase Requisition Java Added

---
+++ src/SalesManager/DisplayPurchaseRequisition.java
@@ -0,0 +1,31 @@
+package pom.assignment;
+
+import SalesManager.CreatePurchaseRequisition;
+
+public class DisplayPurchaseRequisition {
+    public void display(CreatePurchaseRequisition requisition){
+    System.out.println("Generating Purchase Requisition...");
+    try {
+            // Sleep for 3 seconds (3000 milliseconds)
+            Thread.sleep(3000);
+        } catch (InterruptedException e){
+            // Handle the exception if necessary
+            e.printStackTrace();
+        }
+    System.out.println("Complete!");
+    System.out.println("+----------------------------------------+");
+    System.out.println("|           Purchase Requisition         |");
+    System.out.println("+----------------------------------------+");
+    System.out.println(  "|Name          | " + requisition.getName());
+    System.out.println(  "|Company       | " + requisition.getCompany());
+    System.out.println(  "|Item Name     | " + requisition.getitemName());
+    System.out.println(  "|Item Code     | " + requisition.getitemCode());
+    System.out.println(  "|Quantity      | " + requisition.getQuantity());
+    System.out.println(  "|Unit Price    | RM" + requisition.getPrice());
+    System.out.println(  "|Total Price   | RM" + requisition.calculateTotalPrice());
+    System.out.println(  "|Required Date | " + requisition.getRequiredDate());
+    System.out.println(  "|Purpose       | " + requisition.getPurpose());
+    System.out.println("+----------------------------------------+");
+    }
+
+}
