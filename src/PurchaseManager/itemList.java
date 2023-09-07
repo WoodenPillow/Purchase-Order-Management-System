38151ea164ce0496773f4044f5662a4c5b7396d9
vince <vince@DESKTOP-N74TA4F>
2023-09-08 01:08:11 +0800

    itemList & supplierList Done

--- src/PurchaseManager/itemList.java
+++ src/PurchaseManager/itemList.java
@@ -1,5 +1,34 @@
 package PurchaseManager;
 
+import java.io.*;
+import java.util.*;
+
 public class itemList {
-    
+    private static final String FILE_PATH = "C:\\Users\\vince\\OneDrive\\Documents\\NetBeansProjects\\Purchase-Order-Management-System\\src\\SalesManager\\Item.txt";
+
+    public static void main(String[] args) {
+        viewItems();
+    }
+
+    private static void viewItems() {
+        System.out.println("\n============================================================");
+        System.out.println("\tList of Items");
+        System.out.println("\n============================================================");
+        System.out.printf("%-5s %-15s %-10s %-10s %-15s%n", "ID", "Name", "Price", "Quantity", "Supplier ID");
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] itemFields = line.split(",");
+                if (itemFields.length == 5) {
+                    System.out.printf("%-5s %-15s %-10s %-10s %-15s%n",
+                            itemFields[0], itemFields[1], itemFields[2], itemFields[3], itemFields[4]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+
---
+++ src/PurchaseManager/supplierList.java
@@ -0,0 +1,32 @@
+package PurchaseManager;
+
+import java.io.*;
+import java.util.*;
+
+public class supplierList {
+    private static final String FILE_PATH = "C:\\Users\\vince\\OneDrive\\Documents\\NetBeansProjects\\Purchase-Order-Management-System\\src\\SalesManager\\Supplier.txt";
+
+    public static void main(String[] args) {
+        viewSuppliers();
+    }
+
+    private static void viewSuppliers() {
+        System.out.println("\n============================================================");
+        System.out.println("\tList of Suppliers");
+        System.out.println("\n============================================================");
+        System.out.printf("%-10s %-20s %-20s %-20s%n", "ID", "Name", "Item ID", "Supplier Country");
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] supplierFields = line.split(",");
+                if (supplierFields.length == 4) {
+                    System.out.printf("%-10s %-20s %-20s %-20s%n",
+                    supplierFields[0], supplierFields[1], supplierFields[2], supplierFields[3]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            }
+    }
+}
